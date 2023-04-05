da409bdb291c22fbdc619c4efa78ad8d2efbc4a7
PedroTejon <pedrohtejon@gmail.com>
2023-04-05 16:42:52 -0300

    Testei com outros algoritmos (ainda compensa mais ficar com rfc) e adicionei dados de idade faltando com base em uma média

---
+++ filler_age.py
@@ -0,0 +1,55 @@
+from csv import reader, writer
+
+def main():
+    with open('train.csv', 'r') as f:
+        dados_treino = [x for x in reader(f)][1:]
+    with open('test.csv', 'r') as f:
+        dados_teste = [x for x in reader(f)][1:]
+
+    # Adicionar idade com base na média de pronomes de tratamento
+    # print(set([linha[3].split(', ')[1].split(' ')[0] for linha in dados_treino] + [linha[2].split(', ')[1].split(' ')[0] for linha in dados_teste]))
+    pronomes = ['Lady.', 'Jonkheer.', 'Dona.', 'Mrs.', 'Capt.', 'Dr.', 'Sir.', 'Mlle.',
+                'Col.', 'Major.', 'Rev.', 'Don.', 'Mme.', 'Mr.', 'Miss.', 'Master.', 'Ms.']
+
+
+    soma = {pronome: [0, 0] for pronome in pronomes}
+    for linha in dados_teste:
+        pronome = linha[2].split(', ')[1].split(' ')[0]
+        if linha[4] != '' and pronome in soma:
+            soma_ant = soma[pronome][0]
+            qntd_ant = soma[pronome][1]
+
+            soma[pronome] = [soma_ant + float(linha[4]), qntd_ant + 1]
+
+    for linha in dados_treino:
+        pronome = linha[3].split(', ')[1].split(' ')[0]
+        if linha[5] != '' and pronome in soma:
+            soma_ant = soma[pronome][0]
+            qntd_ant = soma[pronome][1]
+
+            soma[pronome] = [soma_ant + float(linha[5]), qntd_ant + 1]
+
+    media = {pronome: soma[pronome][0] / soma[pronome][1] for pronome in soma}
+
+    for linha in dados_treino:
+        if linha[5] == '':
+            linha[5] = f'{media[linha[3].split(", ")[1].split(" ")[0]]:.2f}'
+    
+    for linha in dados_teste:
+        if linha[4] == '':
+            linha[4] = f'{media[linha[2].split(", ")[1].split(" ")[0]]:.2f}'
+
+    with open('train edited.csv', 'w') as f:
+        f.write('PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked\n')
+
+        for linha in dados_treino:
+            f.write(','.join(linha[:3]) + f',"{linha[3]}",' + ','.join(linha[4:]) + '\n')
+
+    with open('test edited.csv', 'w') as f:
+        f.write('PassengerId,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked\n')
+
+        for linha in dados_teste:
+            f.write(','.join(linha[:2]) + f',"{linha[2]}",' + ','.join(linha[3:]) + '\n')
+    
+if __name__ == '__main__':
+    main()
--- titanic.py
+++ titanic.py
@@ -1,4 +1,5 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.tree import DecisionTreeClassifier
 from numpy import concatenate
 from pandas import read_csv, get_dummies, DataFrame, concat
 
@@ -14,7 +15,7 @@ def prever(dados_treino, dados_teste, colunas):
     for coluna in colunas_faltando:
         sob_categorizados[coluna] = False
 
-    modelo = RandomForestClassifier(n_estimators=200, random_state=1)
+    modelo = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
     modelo.fit(sob_categorizados, dados_treino['Survived'])
     dados_teste['Survived'] = modelo.predict(sob_teste_categorizados)
 
@@ -22,21 +23,15 @@ def prever(dados_treino, dados_teste, colunas):
 
 
 def main():
-    dados_treino = read_csv('train.csv')
+    dados_treino = read_csv('train edited.csv')
     dados_treino['Cabin'] = [x[0] if type(x) is str else None for x in dados_treino['Cabin']]
-    dados_teste = read_csv('test.csv')
+    dados_teste = read_csv('test edited.csv')
     dados_teste['Cabin'] = [x[0] if type(x) is str else None for x in dados_teste['Cabin']]
-    dados_genero = read_csv('gender_submission.csv')
 
-    result_c = prever(dados_treino.query('not Cabin.isnull()'), dados_teste.query('not Cabin.isnull()'), ['Pclass', 'Sex', 'SibSp', 'Parch', 'Cabin', 'Fare'])
-    result_s = prever(dados_treino.query('Cabin.isnull()'), dados_teste.query('Cabin.isnull()'), ['Pclass', 'Sex', 'SibSp', 'Parch', 'Fare'])
-
-    previsoes = concat([result_c, result_s])
-    previsoes.sort_values(by=['PassengerId'], inplace=True)
+    previsoes = prever(dados_treino, dados_teste, ['Pclass', 'Sex', 'SibSp', 'Parch', 'Fare', 'Age'])
 
     res_submissao = DataFrame({'PassengerId': previsoes.PassengerId, 'Survived': previsoes.Survived})
-
-    res_submissao.to_csv('saida/submission5.csv', index=False)
+    res_submissao.to_csv('saida/submission7.csv', index=False)
 
 
 if __name__ == '__main__':
