e8617cac4e6c23b4b9b3624be4b9ae2772582607
PedroTejon <pedrohtejon@gmail.com>
2023-04-05 15:17:51 -0300

    Refatoração e agora considero as colunas de Cabin e Passenger Fare

--- titanic.py
+++ titanic.py
@@ -1,29 +1,43 @@
 from sklearn.ensemble import RandomForestClassifier
-from pandas import read_csv, get_dummies, DataFrame
-from os import listdir
+from numpy import concatenate
+from pandas import read_csv, get_dummies, DataFrame, concat
 
-dados_treino = read_csv('train.csv')
-dados_teste = read_csv('test.csv')
-dados_genero = read_csv('gender_submission.csv')
 
-sobreviventes = dados_treino['Survived']
+def prever(dados_treino, dados_teste, colunas):
+    sob_categorizados = get_dummies(dados_treino[colunas])
+    sob_teste_categorizados = get_dummies(dados_teste[colunas])
 
-colunas = ['Pclass', 'Sex', 'SibSp', 'Parch']
+    colunas_faltando = set(sob_categorizados.columns).difference(set(sob_teste_categorizados.columns))
+    for coluna in colunas_faltando:
+        sob_teste_categorizados[coluna] = False
+    colunas_faltando = set(sob_teste_categorizados.columns).difference(set(sob_categorizados.columns))
+    for coluna in colunas_faltando:
+        sob_categorizados[coluna] = False
 
-sob_categorizados = get_dummies(dados_treino[colunas])
-sob_teste_categorizados = get_dummies(dados_teste[colunas])
+    modelo = RandomForestClassifier(n_estimators=200, random_state=1)
+    modelo.fit(sob_categorizados, dados_treino['Survived'])
+    dados_teste['Survived'] = modelo.predict(sob_teste_categorizados)
 
-modelo = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
-modelo.fit(sob_categorizados, sobreviventes)
+    return dados_teste
 
-previsoes = modelo.predict(sob_teste_categorizados)
 
-res_submissao = DataFrame({'PassengerId': dados_teste.PassengerId, 'Survived': previsoes})
-res_visualizacao = DataFrame({'ID': dados_teste.PassengerId, 'Sobreviveu': ['Sim' if x else 'Não' for x in previsoes], 
-                          'Classe': [['Primeira', 'Segunda', 'Terceira'][x - 1] for x in dados_teste.Pclass], 
-                          'Sexo': ['Homem' if x else 'Mulher' for x in dados_teste.Sex], 
-                          'Qntd. Parentes': dados_teste.SibSp,
-                          'Qntd. Filhos/Pais': dados_teste.Parch})
+def main():
+    dados_treino = read_csv('train.csv')
+    dados_treino['Cabin'] = [x[0] if type(x) is str else None for x in dados_treino['Cabin']]
+    dados_teste = read_csv('test.csv')
+    dados_teste['Cabin'] = [x[0] if type(x) is str else None for x in dados_teste['Cabin']]
+    dados_genero = read_csv('gender_submission.csv')
 
-res_submissao.to_csv('saida/submission.csv', index=False)
-res_visualizacao.to_csv('saida/full_results.csv', index=False)
+    result_c = prever(dados_treino.query('not Cabin.isnull()'), dados_teste.query('not Cabin.isnull()'), ['Pclass', 'Sex', 'SibSp', 'Parch', 'Cabin', 'Fare'])
+    result_s = prever(dados_treino.query('Cabin.isnull()'), dados_teste.query('Cabin.isnull()'), ['Pclass', 'Sex', 'SibSp', 'Parch', 'Fare'])
+
+    previsoes = concat([result_c, result_s])
+    previsoes.sort_values(by=['PassengerId'], inplace=True)
+
+    res_submissao = DataFrame({'PassengerId': previsoes.PassengerId, 'Survived': previsoes.Survived})
+
+    res_submissao.to_csv('saida/submission5.csv', index=False)
+
+
+if __name__ == '__main__':
+    main()
